b5befdf38023e86859fd8658ff0be7aee736fe81
Niranjan <niranjanlt25@gmail.com>
2024-10-14 12:16:18 +0530

    updated_code

--- Hybrid_Frame_Work/src/test/java/com/DemowebShopUtility/ExtentManager.java
+++ Hybrid_Frame_Work/src/test/java/com/DemowebShopUtility/ExtentManager.java
@@ -3,35 +3,49 @@ package com.DemowebShopUtility;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
-public class ExtentManager 
-{    private static ExtentReports reports;
-	private static ThreadLocal<ExtentTest> test = new ThreadLocal();
-
+public class ExtentManager
+{ 
+	public static ExtentReports reports;
+	
+	
+	
+	
+	
+	
+	 //creating the thread Local to  initiate test
+	public static ThreadLocal<ExtentTest> test = new ThreadLocal();
+	// report name with extention
 	static String path="login.html";
-
-public static ExtentReports getInstance() 
-{
-    if (reports == null) {
+	// calling instance of Extent_report
+	
+	
+	public static ExtentReports getInstance() 
+	{
+    if (reports == null) 
+    {
         reports = new ExtentReports(path);
     }
     return reports;
-}
-
-public static ExtentTest getTest() {
+	}
+	//calling the 
+public static ExtentTest getTest() 
+{
     return test.get();
 }
-
-public static void setTest(ExtentTest extentTest) {
+public static void setTest(ExtentTest extentTest) 
+{
     test.set(extentTest);
 }
 
-public static void endTest() {
-    if (getTest() != null) {
+public static void endTest() 
+{
+    if (getTest() != null) 
+    {
         getInstance().endTest(getTest());
     }
 }
-
-public static void flush() {
+public static void flush() 
+{
     getInstance().flush();
 }
 }
